d82fa503b497f85f78187aab58ecacc2d45b44f7
adchejjari <a.chejjari@gmail.com>
2023-02-16 14:37:42 +0100

    create node class with find method, getters and setters

--- Btree.java
+++ Btree.java
@@ -1,3 +1,9 @@
 public class Btree {
+    Node root;
+    int m;
 
+    public Btree(int _m) {
+        this.m = _m;
+        root = new Node(_m);
+    }
 }
--- Node.java
+++ Node.java
@@ -1,3 +1,31 @@
 public class Node {
 
+    private int m;
+    private int keys[];
+    private Node child[];
+    private boolean isLeaf;
+
+    public Node(int _m) {
+        this.m = _m;
+        this.keys = new int[m - 1];
+        this.child = new Node[m];
+        this.isLeaf = true;
+    }
+
+    public boolean isLeaf() {
+        return this.isLeaf;
+    }
+
+    public void setLeaf(boolean v) {
+        this.isLeaf = v;
+    }
+
+    public int findKey(int key) {
+        for (int i = 0; i < this.m - 1; i++) {
+            if (this.keys[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
